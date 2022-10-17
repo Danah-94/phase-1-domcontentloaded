9170d249915b7e55d9ae12538426d783dae46bec
Danah <osta.danah@gmail.com>
2022-10-18 00:32:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+// change the text on the page when DOM content has loaded
+document.addEventListener("DOMContentLoaded", function() {
+    let p = document.querySelector("#text");
+    p.textContent = "This is really cool!";
+  });
